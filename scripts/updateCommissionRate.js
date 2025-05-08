218cb10122af13f71ff56585a63a7e75f513eccd
kr-nitya <nitya.m@knackroot.com>
2025-05-08 12:36:29 +0530

    Add functionality to update validator commission rate

--- scripts/updateCommissionRate.js
+++ scripts/updateCommissionRate.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 require("dotenv").config()
 const { ethers } = require("ethers")
 
@@ -60,9 +61,45 @@ async function updateCommissionRate(amount) {
         const receipt = await tx.wait()
         console.log("✅ Transaction confirmed in block:", receipt.blockNumber)
 
+        await updateValidatorCommission(validatorAddress);
+        console.log("✅ Validator commission rate updated");
     } catch (err) {
         console.error("❌ Error:", err.message || err)
     }
 }
 
-updateCommissionRate(amount)
+async function updateValidatorCommission(validatorAddress) {
+  try {
+    const backendUrl = process.env.STAKING_BACKEND_URL;
+
+    if (!backendUrl) {
+      throw new Error(
+        "STAKING_BACKEND_URL is not defined in the environment variables."
+      );
+    }
+
+    if (!validatorAddress) {
+      throw new Error(
+        "Validator address is not provided. Please pass it as a command-line argument."
+      );
+    }
+
+    await axios({
+      method: "POST",
+      url: `${backendUrl}/api/validator/create-or-update-validator`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        validatorWalletAddress: validatorAddress,
+      },
+    });
+  } catch (error) {
+    console.error(
+      `An error occurred while updating validator commission rate for address: ${validatorAddress}`,
+      error
+    );
+  }
+}
+
+updateCommissionRate(amount);
